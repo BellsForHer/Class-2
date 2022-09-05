e91945a937dbf9d97a5452122bd489043486770f
BellsForHer <arhall88@gmail.com>
2022-09-05 16:36:58 -0500

    Update excericise1.js

---
+++ excericise1.js
@@ -0,0 +1,30 @@
+//string
+let useer = "arhall88";
+
+//number
+let a = 10;
+
+//boolean 
+let z = 100;
+
+console.log(80 > z); 
+
+//null
+let b = null;
+
+//undefined
+let d = undefined;
+
+//object
+const user = {
+    name: 'Susan',
+    Occupation: 'Lawyer',
+    Car: 'Honda Civic'
+
+};
+
+//array
+const carMakers = ["Honda", "Toyota", "Fiat"];
+
+//No default value
+let number = undefined
